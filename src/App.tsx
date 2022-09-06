84a624bfc9ca95c5e6858f312114bde1e7177618
qiaoguosheng <294238921@qq.com>
2022-09-06 18:21:31 +0800

    setState异步更新，同步处理

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
+import React from "react";
 import logo from "./assets/images/logo.svg";
-import robotData from './mockData/robot.json'
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 import styles from "./App.module.css";
 
+interface Props {}
+interface State {
+  robotList: any[],
+  count: number
+}
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <h1>超级无敌可爱机器人商城网站就是要o(╥﹏╥)o</h1>
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      robotList: [],
+      count: 0
+    }
+  }
+
+  componentDidMount(): void {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((res) => this.setState({ robotList: res}))
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>超级无敌可爱机器人商城网站就是要o(╥﹏╥)o</h1>
+        </div>
+        <button
+          onClick={() => {
+            this.setState(
+              (prevState) => {
+                return { count: prevState.count + 1 }
+              }
+            )
+            
+             this.setState(
+               (prevState) => {
+                 return { count: prevState.count + 1 }
+               }
+             )
+          }}
+        >
+          add {this.state.count}
+        </button>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotList.map((r) => (
+            <Robot id={r.id} key={r.id} name={r.name} email={r.email} />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotData.map((r) => (
-          <Robot id={r.id} key={r.id} name={r.name} email={r.email} />
-        ))}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
